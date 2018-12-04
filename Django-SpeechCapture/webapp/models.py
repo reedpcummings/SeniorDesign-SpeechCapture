9ce24a4f75e5075fe1e496223f01ba4a6e2a60d7
James Lussier <ragnar0kx@live.com>
2018-12-04 03:45:59 -0600

    Database Added, removed unused files

--- Django-SpeechCapture/webapp/models.py
+++ Django-SpeechCapture/webapp/models.py
@@ -1,21 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.conf import settings
 
+
 class Recordings(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     url = models.CharField(max_length=1000)
     name = models.CharField(max_length=1000)
     uploadDate = models.DateTimeField(max_length=100)
+
 
 class Transcriptions(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     url = models.CharField(max_length=1000)
     name = models.CharField(max_length=1000)
     uploadDate = models.DateTimeField(max_length=100)
 
+
 class Analysis(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     url = models.CharField(max_length=1000)
     name = models.CharField(max_length=1000)
     uploadDate = models.DateTimeField(max_length=100)
